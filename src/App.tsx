d4f998c87677eb2415528fd7dc7592bb48d6c139
Student <student@atyrau.itstep>
2023-09-01 20:10:28 +0600

    project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from "react";
+import "./App.css";
 
 function App() {
+  const todos = ["Frontend", "Dinner", "English", "Learn"];
+
+  const [list, setList] = useState([
+    "Изучить JavaScript",
+    "Изучить паттерны проектирования",
+    "ReactJS Hooks (useState, useReducer, useEffect и т.д.)",
+    "Redux (redux-observable, redux-saga)",
+  ]);
+
+  const [newList, setNewList] = useState("");
+
+  const takeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { target } = e;
+    setNewList(target.value);
+  };
+
+  const addList = (key: any) => {
+    if (key.code === "Enter") {
+      list.push(newList);
+    }
+    setNewList("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="modal-view">
+        <div className="mv-left">
+          <h4>Все задачи</h4>
+          {todos.map((t: string) => (
+            <li>{t}</li>
+          ))}
+          <div className="mv-add">
+            <span>+</span> Добавить папку
+          </div>
+        </div>
+        <div className="mv-right">
+          <h1>Frontend</h1>
+          <div className="mv-super-input">
+            <input
+              onKeyDown={(key) => addList(key)}
+              onChange={(e) => takeChange(e)}
+              type="text"
+            />
+          </div>
+          {list.map((l: string) => (
+            <div className="mv-list">
+              <input type="checkbox" /> {l}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
